f928c1abe35893e25c3fa39156b282ff9b0b0d47
Kaizers08 <regalajaireellson@gmail.com>
2025-09-23 19:31:03 +0800

    new

---
+++ src/app/try-lang/try-lang.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-try-lang',
+  imports: [],
+  templateUrl: './try-lang.html',
+  styleUrl: './try-lang.scss'
+})
+export class TryLang {
+
+}
