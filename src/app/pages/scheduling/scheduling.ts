53de09146082ec1170ac6f84541db870388d238c
Kaizers08 <regalajaireellson@gmail.com>
2025-09-23 20:28:24 +0800

    new

---
+++ src/app/pages/scheduling/scheduling.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scheduling',
+  imports: [],
+  templateUrl: './scheduling.html',
+  styleUrl: './scheduling.scss'
+})
+export class Scheduling {
+
+}
